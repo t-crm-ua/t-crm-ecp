d42325d4008c407d570640f358a0bd11cd44ab93
Kirill Smirnov <krl.smirnov.work@gmail.com>
2021-09-02 09:49:55 +0300

    added acsk ukraine

--- src/ACSKEnum.php
+++ src/ACSKEnum.php
@@ -8,11 +8,13 @@ class ACSKEnum
     const IDD = 'acskidd.gov.ua';
     const IIT = 'ca.iit.com.ua';
     const K_SYSTEMS = 'ca.ksystems.com.ua';
+    const UKRAINE = 'uakey.com.ua';
 
     const ACKS_TYPES = [
         self::PRIVATBANK => 'АЦСК АТ КБ «ПРИВАТБАНК»',
         self::IDD => 'КНЕДП - ІДД ДПС',
         self::IIT => 'АТ «ІНСТИТУТ ІНФОРМАЦІЙНИХ ТЕХНОЛОГІЙ',
-        self::K_SYSTEMS => 'АЦСК ТОВ «Ключові системи»'
+        self::K_SYSTEMS => 'АЦСК ТОВ «Ключові системи»',
+        self::UKRAINE => 'АЦСК ТОВ "Центр сертифікації ключів "Україна"'
     ];
 }
