1d8d708b004d28ae4ee979cca8381d21ebf868c3
Kirill Smirnov <krl.smirnov.work@gmail.com>
2021-12-15 14:44:45 +0200

    certificate storage exceptions

--- src/CertificateStorage.php
+++ src/CertificateStorage.php
@@ -2,31 +2,31 @@
 
 namespace AndrewSvirin\EUSPE;
 
-use Exception;
+use CertificateDirectoryException;
 use AndrewSvirin\EUSPE\traits\ExpiredTrait;
 
 class CertificateStorage
 {
-
-  use ExpiredTrait;
-
-  protected $dir;
-
-  public function __construct(string $dir)
-  {
-    $this->dir = $dir;
-  }
-
-  /**
-   * @param User $user
-   * @return Certificate
-   * @throws Exception
-   */
-  public function prepare(User $user): Certificate
-  {
-    $cert = new Certificate("{$this->dir}/{$user->getServerHost()}/{$user->getUserName()}");
-    $cert->configure();
-    return $cert;
-  }
-
+    
+    use ExpiredTrait;
+    
+    protected $dir;
+    
+    public function __construct(string $dir)
+    {
+        $this->dir = $dir;
+    }
+    
+    /**
+     * @param  User  $user
+     * @return Certificate
+     * @throws CertificateDirectoryException
+     */
+    public function prepare(User $user): Certificate
+    {
+        $cert = new Certificate("{$this->dir}/{$user->getServerHost()}/{$user->getUserName()}");
+        $cert->configure();
+        return $cert;
+    }
+    
 }
