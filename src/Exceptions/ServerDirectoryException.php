ae8632dc54403c6d5f662b059485ca52779af8e8
Kirill Smirnov <krl.smirnov.work@gmail.com>
2021-12-15 14:48:05 +0200

    server directory exception

---
+++ src/Exceptions/ServerDirectoryException.php
@@ -0,0 +1,6 @@
+<?php
+
+class ServerDirectoryException extends Exception
+{
+    
+}
--- src/Server.php
+++ src/Server.php
@@ -3,60 +3,61 @@
 namespace AndrewSvirin\EUSPE;
 
 use Exception;
+use ServerDirectoryException;
 
 /**
  * Class Server implements server for client configuration.
  */
 class Server
 {
-
-  /**
-   * @var string
-   */
-  private $dir;
-
-  public function __construct($dir)
-  {
-    $this->dir = $dir;
-  }
-
-  /**
-   * Prepare servers dir.
-   * @throws Exception
-   */
-  public function configure(): void
-  {
-    if (!file_exists($this->dir) && !(mkdir($this->dir, 0777, true))) {
-      throw new Exception('Can not prepare servers dir.');
+    
+    /**
+     * @var string
+     */
+    private $dir;
+    
+    public function __construct($dir)
+    {
+        $this->dir = $dir;
     }
-  }
-
-  /**
-   * Open connection.
-   * @throws Exception
-   */
-  public function open(): void
-  {
-    $dir = realpath($this->dir);
-    putenv(sprintf('LD_LIBRARY_PATH=%s', $dir));
-  }
-
-  /**
-   * Close connection.
-   */
-  public function close(): void
-  {
-    $dir = sprintf('%s/servers/default', __DIR__);
-    putenv(sprintf('LD_LIBRARY_PATH=%s', $dir));
-  }
-
-  public function getOSPLMConfigPath(): string
-  {
-    return sprintf('%s/osplm.ini', $this->dir);
-  }
-
-  public function getOSPCUConfigPath(): string
-  {
-    return sprintf('%s/ospcu.ini', $this->dir);
-  }
+    
+    /**
+     * Prepare servers dir.
+     * @throws ServerDirectoryException
+     */
+    public function configure(): void
+    {
+        if (!file_exists($this->dir) && !(mkdir($this->dir, 0777, true))) {
+            throw new ServerDirectoryException('Can not prepare servers dir.');
+        }
+    }
+    
+    /**
+     * Open connection.
+     * @throws Exception
+     */
+    public function open(): void
+    {
+        $dir = realpath($this->dir);
+        putenv(sprintf('LD_LIBRARY_PATH=%s', $dir));
+    }
+    
+    /**
+     * Close connection.
+     */
+    public function close(): void
+    {
+        $dir = sprintf('%s/servers/default', __DIR__);
+        putenv(sprintf('LD_LIBRARY_PATH=%s', $dir));
+    }
+    
+    public function getOSPLMConfigPath(): string
+    {
+        return sprintf('%s/osplm.ini', $this->dir);
+    }
+    
+    public function getOSPCUConfigPath(): string
+    {
+        return sprintf('%s/ospcu.ini', $this->dir);
+    }
 }
